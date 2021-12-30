8f8926dd5378e0146d0370e70bd2eeb00e8f66a2
Anibal Montero <cubanibal@hotmail.com>
2021-12-30 11:01:31 +0100

    Instalados stock_quote y devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
   def index
-  end
+     @api = StockQuote::Stock.new(api_key: 'pk_9a3fffc17adf4052ac921fb0e57968bc')
+    if params[:simbolo] == ""
+      @nada = "Ehh! Has olvidado entrar la búsqueda"
+    elsif params[:simbolo]
+      begin
+        @bolsa = StockQuote::Stock.quote( params[:simbolo] )
+      rescue => e
+        @bolsa = nil
+      end
+      if @bolsa == nil
+          @error = "Ehh! Ese símbolo no existe" 
+      end
+    end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :usuarios
   root 'home#index'
   
+  post "/" => 'home#index'
 end
