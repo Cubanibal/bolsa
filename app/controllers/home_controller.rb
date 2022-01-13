d1d2496e27a9046ee76aeb05d149796b0a984f00
Anibal Montero <cubanibal@hotmail.com>
2022-01-13 23:09:04 +0100

    Bootstrap para las páginas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,20 @@
 class HomeController < ApplicationController
   def index
      @api = StockQuote::Stock.new(api_key: 'pk_9a3fffc17adf4052ac921fb0e57968bc')
+
     if params[:simbolo] == ""
       @nada = "Ehh! Has olvidado entrar la búsqueda"
     elsif params[:simbolo]
       begin
         @bolsa = StockQuote::Stock.quote( params[:simbolo] )
+        @logo = StockQuote::Stock.logo( params[:simbolo] )
+        @compania = StockQuote::Stock.company( params[:simbolo] ) 
+        @noticias = StockQuote::Stock.news( params[:simbolo] )
+        
       rescue => e
         @bolsa = nil
+        @logo = nil
+        @compania = nil
       end
       if @bolsa == nil
           @error = "Ehh! Ese símbolo no existe" 
--- app/models/empresa.rb
+++ app/models/empresa.rb
@@ -1,3 +1,5 @@
 class Empresa < ApplicationRecord
 	belongs_to :usuario
+	validates :simbolo, presence: true
+	validates :simbolo, format: {without: /\s/}  
 end
